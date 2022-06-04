50f0619ed18f231ce2739633acd81c6303f43575
Omer Elmaliach <omerelmaliachprivate@gmail.com>
2022-06-04 21:03:01 +0300

    Redo test code

--- core_app/sharer.py
+++ core_app/sharer.py
@@ -10,9 +10,9 @@ class Sharer(object):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.s.bind((socket.gethostbyname(socket.gethostname()), 8886))
 
-        self.start_work()
+        self.send_image()
 
-    """def send_image(self):
+    def send_image(self):
         while self:
             mss().shot(mon=1, output='screen.png')
             with open('screen.png', 'rb') as img:
@@ -20,19 +20,4 @@ class Sharer(object):
                 while image_chunks:
                     self.s.sendto(image_chunks, (self.viewer_addr, 8886))
                     image_chunks = img.read(8192)
-                self.s.sendto(b"DONE", (self.viewer_addr, 8886))"""
-
-    def start_work(self):
-        while True:
-            mss().shot(output="img.png")
-            img = cv2.imread("img.png")
-            img_resized = cv2.resize(img, (1080, 720))
-            cv2.imwrite("img.png", img_resized)
-
-            with open('img.png', 'rb') as screen_image:
-                img_data = screen_image.read(8192)
-                while img_data:
-                    self.s.sendto(img_data, (self.viewer_addr, 8886))
-                    img_data = screen_image.read(8192)
-                self.s.sendto("Image sent!".encode('utf-8'), (self.viewer_addr, 8886))
-                screen_image.close()
+                self.s.sendto(b"DONE", (self.viewer_addr, 8886))
--- core_app/viewer.py
+++ core_app/viewer.py
@@ -12,10 +12,10 @@ class Viewer(object):
 
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.s.bind((socket.gethostbyname(socket.gethostname()), 8886))
-        self.thread = threading.Thread(target=self.recv_img)
+        self.thread = threading.Thread(target=self.display_screen)
         self.thread.start()
 
-    """def display_screen(self):
+    def display_screen(self):
         while self:
             with open('client_screen.png', 'wb') as img:
                 image_chunks = self.s.recvfrom(8192)[0]
@@ -23,23 +23,4 @@ class Viewer(object):
                     img.write(image_chunks)
                     image_chunks = self.s.recvfrom(8192)[0]
             self.window.display_image('client_screen.png')
-            os.remove('client_screen.png')"""
-
-    def recv_img(self):
-        while True:
-            rewrite_data = None
-            if os.path.isfile('D:\images\img.png'):
-                os.remove('D:\images\img.png')
-            img = open('D:\images\img.png', 'wb')
-            while True:
-                data = self.s.recv(8192)
-                if rewrite_data is None:
-                    rewrite_data = data
-                else:
-                    rewrite_data = rewrite_data + data
-                if "Image sent!".encode('utf-8') in data:
-                    img.write(rewrite_data)
-                    img.close()
-
-                    self.window.display_image('D:\images\img.png')
-                    break
+            os.remove('client_screen.png')
